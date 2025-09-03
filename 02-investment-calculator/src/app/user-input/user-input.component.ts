e65d36c13f88ecd9504b92466a9d698533be424b
Damian Cozma <damiancozma620@gmail.com>
2025-09-03 12:58:37 +0300

    implement investment results algorithm

--- 02-investment-calculator/src/app/user-input/user-input.component.ts
+++ 02-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, output} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {InvestmentInput} from "../investment-input.model";
 
 @Component({
   selector: 'app-user-input',
@@ -10,15 +11,19 @@ import {FormsModule} from "@angular/forms";
 })
 export class UserInputComponent {
 
+  calculate = output<InvestmentInput>();
+
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    console.log(this.enteredDuration);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredInitialInvestment);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
   }
 }
