3dd74345a42a9002be855e613b91de5cd0aa9070
Damian Cozma <damiancozma620@gmail.com>
2025-09-09 11:35:29 +0300

    components deep dive

--- 03-support-app/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 03-support-app/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,18 +1,17 @@
-import {Component, input, Input} from '@angular/core';
+import {Component, input, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  host: {
+    class: 'dashboard-item',
+  }
+
 })
 export class DashboardItemComponent {
-  // @Input({required: true}) image!: {
-  //   src: string;
-  //   alt: string;
-  // };
-  // @Input({required: true}) title!: string;
 
   image = input.required<{
     src: string;
--- 03-support-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-support-app/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "../../../shared/button/button.component";
+import {ControlComponent} from "../../../shared/control/control.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    ControlComponent,
+    FormsModule
+  ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+
+  onSubmit(titleElement: string, textInput: string) {
+    console.log(titleElement);
+    console.log(textInput);
+  }
 
 }
