2748d109064635930b2ac6af811699ef263d79c0
Damian Cozma <damiancozma620@gmail.com>
2025-09-10 13:25:12 +0300

    custom directives

---
+++ 05-learning-app/src/app/auth/auth.directive.ts
@@ -0,0 +1,26 @@
+import {Directive, effect, inject, input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Permission} from "./auth.model";
+import {AuthService} from "./auth.service";
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true
+})
+export class AuthDirective {
+
+  userType = input.required<Permission>({alias: 'appAuth'});
+  private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef); // template content
+  private viewContainerRef = inject(ViewContainerRef); // position of the template in the DOM
+
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainerRef.clear();
+      }
+    });
+  }
+
+}
