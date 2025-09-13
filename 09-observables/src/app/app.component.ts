552b395f228f58aac76e409f6454a9ca8b776bfa
Damian Cozma <damiancozma620@gmail.com>
2025-09-14 00:13:39 +0300

    observable operators

--- 09-observables/src/app/app.component.ts
+++ 09-observables/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
+import {interval, map} from 'rxjs'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+    });
+  }
+
+  ngOnInit() {
+    const subscription = interval(1000).pipe(
+      map((val) => val * 2)
+    ).subscribe({
+      next: value => console.log(value),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
 
 }
