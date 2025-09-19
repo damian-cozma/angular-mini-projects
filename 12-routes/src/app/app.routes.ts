aaeb554a479f4e183307935c56eb816174836e29
Damian Cozma <damiancozma620@gmail.com>
2025-09-19 04:34:16 +0300

    not found component & asc/desc order feature

--- 12-routes/src/app/app.routes.ts
+++ 12-routes/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import {Routes} from "@angular/router";
 import {TasksComponent} from "./tasks/tasks.component";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
+import {UserTasksComponent} from "./users/user-tasks/user-tasks.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {routes as userRoutes} from './users/user.routes';
 
 export const routes: Routes = [
   {
@@ -8,7 +12,12 @@ export const routes: Routes = [
     component: NoTaskComponent,
   },
   {
-    path: 'tasks',
-    component: TasksComponent,
+    path: 'users/:userId',
+    component: UserTasksComponent,
+    children: userRoutes,
   },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
 ]
